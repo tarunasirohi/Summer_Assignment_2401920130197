33d0510843741501a6099ffc41c216c435c7c0c8
tarunasirohi <tarunasirohi89@gmail.com>
2026-06-23 21:54:11 +0530

    Time: 128 ms (5.35%) | Memory: 94.9 MB (5.33%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+         map<int,int> mpp;
+        for(int i = 0;i<nums.size();i++){
+            mpp[nums[i]] += 1;
+        }
+        for(auto j : mpp){
+            if(j.second >= 2)
+              return true;
+        }
+        return false;
+       
+    }
+};
