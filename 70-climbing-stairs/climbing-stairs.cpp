3e2501fa07e5558ebc00c6beb0032b484a104b9f
tarunasirohi <tarunasirohi89@gmail.com>
2026-07-09 21:48:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,13 +3,13 @@ public:
     int Stairs(int n,vector<int>& DP){
         if(n <= 2)
           return n;
-        if(DP[n] != -1)
-           return DP[n];
-        return DP[n] = Stairs(n-1,DP) + Stairs(n-2,DP);   
+        if(DP[n-1] != -1)
+           return DP[n-1];
+        return DP[n-1] = Stairs(n-1,DP) + Stairs(n-2,DP);   
     }
     int climbStairs(int n) {
 
-        vector<int> DP(n+1,-1);
+        vector<int> DP(n,-1);
         return Stairs(n,DP);
     }
 };
